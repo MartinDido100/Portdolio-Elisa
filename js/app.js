5c038caed0eda11cea00c5903b8e396648429e65
Martin Didolich <marindidolich2012@gmail.com>
2021-06-27 23:20:10 -0300

    Added observer on proyects

--- js/app.js
+++ js/app.js
@@ -11,3 +11,24 @@ a.forEach( (element, i) =>{
         hover[i].addEventListener("animationend",()=> hover[i].style.animation = "none");
     })
 })
+
+const proyectsPhone = document.querySelectorAll(".main__proyect-phone");
+const proyects = document.querySelectorAll(".main__proyect");
+const proyectsText = document.querySelectorAll(".main__proyect-text");
+
+const verifyVisibility = entries =>{
+    for(let entry of entries){
+        if (entry.isIntersecting) {
+           entry.target.firstChild.nextSibling.classList.add('main__proyect-phone-active');
+           entry.target.lastChild.previousSibling.classList.add('main__proyect-text-active');
+        }else{
+            entry.target.firstChild.nextSibling.classList.remove('main__proyect-phone-active');
+            entry.target.lastChild.previousSibling.classList.remove('main__proyect-text-active');
+
+        }
+    }
+}
+
+const observer = new IntersectionObserver(verifyVisibility);
+
+proyects.forEach(x => observer.observe(x));
