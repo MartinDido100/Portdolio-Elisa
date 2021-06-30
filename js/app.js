6349a2a6bc223dfe97f3dfba6c921de0a6966a72
Martin Didolich <marindidolich2012@gmail.com>
2021-06-30 13:43:29 -0300

    Added form validation

--- js/app.js
+++ js/app.js
@@ -1,16 +1,5 @@
 "use strict";
 
-const hover = document.querySelectorAll(".hover");
-const a = document.querySelectorAll(".effect");
-
-
-a.forEach( (element, i) =>{
-    element.addEventListener("mouseenter", (e)=>{
-        e.preventDefault;
-        hover[i].style.animation = "hoverEffect .5s ease"; 
-        hover[i].addEventListener("animationend",()=> hover[i].style.animation = "none");
-    })
-})
 
 const proyectsPhone = document.querySelectorAll(".main__proyect-phone");
 const proyects = document.querySelectorAll(".main__proyect");
@@ -29,10 +18,12 @@ const observer = new IntersectionObserver(verifyVisibility);
 
 proyects.forEach(x => observer.observe(x));
 
+const contact = document.querySelector(".main__contact");
 const form = document.querySelector(".main__form");
 const text = document.getElementById("text");
 const email = document.getElementById("email");
 const sendButton = document.getElementById("submit");
+const message = document.getElementById("message")
 
 form.addEventListener("input",()=>{
     if (text.value.length > 0 && email.value.length > 0) {
@@ -40,10 +31,75 @@ form.addEventListener("input",()=>{
         sendButton.style.background = "#42E2B8";
         sendButton.style.border = "2px solid black";
         sendButton.style.cursor = "pointer";
+        sendButton.classList.add("main__form-submit-active");
     }else{
         sendButton.setAttribute("disabled","");
+        sendButton.classList.remove("main__form-submit-active");
         sendButton.style.background = "";
         sendButton.style.border = "";
         sendButton.style.cursor = "";
     }
-})
+});
+
+let validatedData = {
+    nombre : false,
+    mail : false
+};
+
+const validateMail = () =>{
+    if(email.value.includes(".com",".","@")){
+        validatedData.mail = true;
+    }else{
+        validatedData.mail = false;
+    }
+}
+
+const validateName = ()=>{
+    if (text.value.length >= 2) {
+        validatedData.nombre = true;
+    }else{
+        validatedData.nombre = false;
+    }
+}
+
+const createP = () => {
+    const p = document.createElement("P");
+    p.classList.add("main__validation-error");
+    return p;
+}
+
+const p = createP();
+
+const checkValidation = () =>{
+    let formValues = Object.values(validatedData);
+    let formKeys = Object.keys(validatedData);
+    let validatedIndex = formValues.findIndex(x => x == false);
+    if (validatedIndex == -1){
+        alert("Formulario enviado");
+        if (contact.lastChild === p) {
+            contact.removeChild(p);
+        }
+        text.value = '';
+        email.value = '';
+        message.value = '';
+    }
+    else{
+        let code = '';
+        for(let key in validatedData){
+            if (validatedData[key] == false) {
+                code += `Por favor complete el ${key}* <br>`;
+            }
+        }
+        p.innerHTML = code;
+        contact.appendChild(p);
+    }
+}
+
+
+
+form.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    validateName();
+    validateMail();
+    checkValidation();
+});
